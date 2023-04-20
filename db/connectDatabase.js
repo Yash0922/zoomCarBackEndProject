6a6c69db337c45729190b364b92db658bb1ae3e8
yash0922 <yashkumarpal987@gmail.com>
2023-04-20 20:47:43 +0530

    update

--- db/connectDatabase.js
+++ db/connectDatabase.js
@@ -5,6 +5,7 @@ mongoose.set("strictQuery", true);
 
 async function connectDatabase() {
   try {
+    console.log(config.MONGODB_URL)
     await mongoose.connect(config.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to database");
   } catch (error) {
